5edbe60058b1bdddc4a1f97500eb902cc6c234ec
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-06-03 15:23:37 +0800

    Added Fragment classes and hooked them up to MainActivity.

---
+++ app/src/main/java/com/training/android/fragmentbasics/InputFragment.java
@@ -0,0 +1,19 @@
+package com.training.android.fragmentbasics;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InputFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_input, container, false);
+        return view;
+    }
+}
